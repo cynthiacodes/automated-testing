11121ce8f2a6ee4a193fd6ebfd54fd77a1f76ad9
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-21 14:35:46 +0100

    updated shortest-string test

--- src/find-shortest-string.test.js
+++ src/find-shortest-string.test.js
@@ -1,11 +1,10 @@
-
 const findShortestString = require("./find-shortest-strings");
 
 test("findShortestString finds the shortest string in an array", () => {
-    expect(findShortestString(["it", "is", "a", "nice", "day"])).tobe("a");
-    expect(findShortestString(["why", "hello", "to", "you"])).tobe("to");
+  expect(findShortestString(["it", "is", "a", "nice", "day"])).tobe("a");
+  expect(findShortestString(["why", "hello", "to", "you"])).tobe("to");
 });
 
 test("findShortestString returns the earlier string in cases of joint shortest strings", () => {
-    expect(findShortestString(["brave", "dance"])).toBe("brave");
-  });
+  expect(findShortestString(["brave", "dance"])).toBe("brave");
+});
