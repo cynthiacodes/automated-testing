a54088e30caecb316ec1b7e9e51712cf30fa2adc
cynthiacodes <cjnkstudy01@hotmail.com>
2023-06-21 14:17:30 +0100

    split the test into two

--- src/find-shortest-string.test.js
+++ src/find-shortest-string.test.js
@@ -4,5 +4,8 @@ const findShortestString = require("./find-shortest-strings");
 test("findShortestString finds the shortest string in an array", () => {
     expect(findShortestString(["it", "is", "a", "nice", "day"])).tobe("a");
     expect(findShortestString(["why", "hello", "to", "you"])).tobe("to");
-    expect(findShortestString(["brave", "dance"])).tobe("brave")
 });
+
+test("findShortestString returns the earlier string in cases of joint shortest strings", () => {
+    expect(findShortestString(["brave", "dance"])).toBe("brave");
+  });
